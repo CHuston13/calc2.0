2a83a116bca9d758960bd9af9b42e87f93b42e24
Chuston13 <Cdhprogrammer@gmail.com>
2022-03-19 12:31:36 -0500

    js.js

--- js.js
+++ js.js
@@ -4,9 +4,19 @@ let buttons = Array.from(document.getElementsByClassName('button'));
 buttons.map(button => {
     button.addEventListener('click',(e)=>{
         switch(e.target.innerText){
-            default:
+            case 'C':
+                display.innerText='';
+                break;
+                //using slice to take the last number from the display
+                case '←':
+                    display.innerText = display.innerText.slice(0,-1);
+                    break;
+                    case '=':
+                        display.innerText = eval(display.innerText);
+                        break;
+                    default:
+//linking contents of buttons into the display 
                 display.innerText += e.target.innerText;
         }
     })
 })
-console.log(buttons)
